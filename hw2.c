d4bc776a19dc4bb0fa3841cbd3c6ef380c5c3f28
Aditya Saraf <sarafa@cs.uw.edu>
2017-01-23 23:19:19 -0800

    added some basic jansson functionality. think i know how to use jansson now for the most part

--- hw2.c
+++ hw2.c
@@ -1,8 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <jansson.h>
 
 #include "Array2D.h"
 
 int main(int argc, char *argv[]) {
-  return EXIT_SUCCESS;
+
+	if (argc != 2)
+	{
+		printf("usage: %s filename\n", argv[0]);
+	}
+	else
+	{
+		json_error_t error;
+		json_t *root = json_load_file(argv[1], 0, &error);
+		json_t *rows = json_object_get(root, "rows");
+		int rowNum = json_integer_value(rows);
+		printf("%i\n", rowNum);
+	}
+	return EXIT_SUCCESS;
 }
